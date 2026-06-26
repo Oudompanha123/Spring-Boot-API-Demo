4f1c1fa45c168527ff75fb509b4b391f0f95ab86
Oudompanha123 <oudompanha456@gmail.com>
2026-06-26 08:09:08 +0700

    Last commit

--- cafe-merchant-api/cm-common/src/main/java/com/ppcb/cafemerchant/common/service/storage/FileStorageService.java
+++ cafe-merchant-api/cm-common/src/main/java/com/ppcb/cafemerchant/common/service/storage/FileStorageService.java
@@ -3,6 +3,7 @@ package com.ppcb.cafemerchant.common.service.storage;
 import com.ppcb.cafemerchant.common.components.constant.StatusCode;
 import com.ppcb.cafemerchant.common.components.exception.BusinessException;
 import com.ppcb.cafemerchant.common.config.properties.FileInfoProperties;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,22 +20,7 @@ public class FileStorageService {
     private final FileInfoProperties fileInfoProperties;
 
     public String storeImage(MultipartFile file, String oldFileName) throws IOException {
-        if (file == null || file.isEmpty()) {
-            throw new BusinessException(StatusCode.FILE_NOT_FOUND);
-        }
-
-        // 10MB = 10 * 1024 * 1024 = 10485760 bytes
-        long maxFileSize = 10 * 1024 * 1024; // 10MB
-        if (file.getSize() > maxFileSize) {
-            throw new BusinessException(StatusCode.FILE_SIZE_EXCEED);
-        }
-
-        String originalFileName = file.getOriginalFilename();
-        if (originalFileName == null || originalFileName.isBlank()) {
-            throw new BusinessException(StatusCode.INVALID_FILE_NAME);
-        }
-
-        String extension = originalFileName.substring(originalFileName.lastIndexOf('.') + 1);
+        String extension = getString(file);
         String newFileName = UUID.randomUUID() + "." + extension;
 
         File targetDir = new File(fileInfoProperties.getServerPath());
@@ -53,6 +39,26 @@ public class FileStorageService {
         Files.copy(file.getInputStream(), newFile.toPath());
 
         return newFileName;
+    }
+
+    private static @NonNull String getString(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            throw new BusinessException(StatusCode.FILE_NOT_FOUND);
+        }
+
+        // 10MB = 10 * 1024 * 1024 = 10485760 bytes
+        long maxFileSize = 10 * 1024 * 1024; // 10MB
+        if (file.getSize() > maxFileSize) {
+            throw new BusinessException(StatusCode.FILE_SIZE_EXCEED);
+        }
+
+        String originalFileName = file.getOriginalFilename();
+        if (originalFileName == null || originalFileName.isBlank()) {
+            throw new BusinessException(StatusCode.INVALID_FILE_NAME);
+        }
+
+        String extension = originalFileName.substring(originalFileName.lastIndexOf('.') + 1);
+        return extension;
     }
 
 }
